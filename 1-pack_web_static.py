1c44e4f9e71f23f159509c1d98e940f4f82516e2
Masalale <c.chomba@alustudent.com>
2025-10-28 16:01:21 +0200

    Refactor Nginx configuration update for improved readability

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Fabric script to generate a .tgz archive from web_static folder
+"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """Generates a .tgz archive from web_static folder"""
+    try:
+        if not os.path.isdir("versions"):
+            os.makedirs("versions")
+        now = datetime.now()
+        archive_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+            now.year, now.month, now.day, now.hour, now.minute, now.second)
+        result = local("tar -cvzf {} web_static".format(archive_name))
+        if result.succeeded:
+            return archive_name
+        else:
+            return None
+    except Exception:
+        return None
